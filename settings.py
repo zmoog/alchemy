59cb94f31810c7f3817104a575752f5aa5ce685b
Maurizio Branca <maurizio.branca@gmail.com>
2011-01-29 16:04:58 +0100

    django 1.3 adjustments and path handling improvements.

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
+import os
 # Django settings for alchemy project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -9,14 +10,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#DATABASE_NAME = '/home/django/domains/alchemy.selfip.biz/alchemy/db/alchemy.db'             # Or path to database file if using sqlite3.
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'alchemy_production'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'alchemy'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES  ={
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db/alchemy.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -54,7 +57,8 @@ SECRET_KEY = 'd()n*zeja_hhr@na6zi(_4db+25ib1v(u8$c7hmsd9n9#=sw0o'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
+    # deprecated: 'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.load_template_source',
 #     'django.template.loaders.eggs.load_template_source',
 )
@@ -73,14 +77,11 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/zmoog/code/django/git/alchemy/templates',
-    #'/srv/django/alchemy/templates',
-    #'/opt/django-trunk/django/contrib/databrowse/templates',
-    '/home/django/domains/alchemy.selfip.biz/alchemy/templates',
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates'),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS =  (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
@@ -97,11 +98,10 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     'django.contrib.webdesign',
     'cash',
-    'gunicorn',
 )
 
-LOGIN_URL = '/accounts/login/'
-LOGOUT_URL = '/accounts/logout/'
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
 LOGIN_REDIRECT_URL = '/'
 
-STATIC_DOC_ROOT = '/home/django/domains/alchemy.selfip.biz/alchemy/_static'
+STATIC_DOC_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '_static')
