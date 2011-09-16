d53af9fc942d1fc90b262b08f63ab5fc3cb5d1a4
Maurizio Branca <maurizio.branca@gmail.com>
2011-09-16 07:57:18 +0200

    * Account balance is calculated on Transfer update in the views (bulk operantions are not affected).  * account_details view is back to a regular read only operation and can now be paginated.  * added transaction annotation.

--- cash/views.py
+++ cash/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect
 from django import forms 
-from django.db.models import Q
+from django.db import transaction
+from django.db.models import Q, Sum
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.views.generic import list_detail
@@ -60,7 +61,7 @@ def transfer_archive_day(request, year, month, day, queryset, paginate_by):
             validity_date__day=int(day)), paginate_by)
 
 
-
+@transaction.commit_on_success()
 def transfer(request, object_id):
     latest = Transfer.objects.order_by('-created_on')[:10] 
     transfer = get_object_or_404(Transfer, pk=object_id)
@@ -69,6 +70,7 @@ def transfer(request, object_id):
         form = TransferForm(request.POST, instance=transfer)   
         if form.is_valid():
             form.save()
+            _update_balance([form.instance.source, form.instance.destination])
             request.user.message_set.create(message="Transfer aggiornato con successo")
             return HttpResponseRedirect(reverse('transfer-detail', kwargs=dict(object_id=transfer.id))) # redirect after post
 
@@ -78,6 +80,7 @@ def transfer(request, object_id):
     return render_to_response('cash/transfer_add.html', {'form': form, 'latest': latest}, context_instance=RequestContext(request))
 
 
+@transaction.commit_on_success()
 def transfer_add(request):
     """
     View for handling the creation/modify of the Transfer objects.
@@ -87,6 +90,7 @@ def transfer_add(request):
         form = TransferForm(request.POST)
         if form.is_valid():
             form.save()
+            _update_balance([form.instance.source, form.instance.destination])
             #request.user.message_set.create(message="%s" % form['notify_recipients'].field)
             return HttpResponseRedirect(reverse('transfer-add')) # redirect after post
     else:
@@ -309,20 +313,6 @@ def account_detail(request, object_id, year=None, month=None):
     transfer_list = Transfer.objects.filter(
             Q(source__id = object_id) | Q(destination__id = object_id)
         ).order_by('-validity_date')
-   
-    # Aggiornamento del bilancio dell'Account
-    # FIXME: occorre sostituire questo metodo con qualcosa di diverso..
-    if not year and not month:
-    
-        account.balance = decimal.Decimal('0')
-
-        for transfer in transfer_list: 
-            if account.id == transfer.destination.id:
-                account.balance += transfer.amount
-            else:
-                account.balance -= transfer.amount
-   
-        account.save()
 
     months = transfer_list.dates('validity_date', 'month', order="DESC")
 
@@ -335,7 +325,8 @@ def account_detail(request, object_id, year=None, month=None):
     context = {
         'now': datetime.datetime.now(),
         'object': account,
-        'transfer_list': transfer_list,
+        'transfer_list': transfer_list[:10],
+        'count': transfer_list.count(),
         'months': months
     }
     
@@ -439,3 +430,27 @@ def _sandbox(request, object_id):
     context = { 'account': account, 'tot': tot }
     
     return render_to_response('cash/sandbox.html', context, context_instance=RequestContext(request))
+
+
+def _update_balance(account_list):
+    """
+    Update the balance for each Account in the list given as a parameter.
+
+    The current implementation uses a sum() aggregation function to obtain INs and OUTs transfer sums
+    and then apply this formula: 
+
+        balance = INs - OUTs.
+
+    """
+
+    for account in account_list:
+ 
+        print('current %s balance is %f' % (account.name, account.balance))
+        ins = Transfer.objects.filter(Q(destination__id=account.id)).aggregate(Sum('amount'))['amount__sum'] or 0
+        outs = Transfer.objects.filter(Q(source__id=account.id)).aggregate(Sum('amount'))['amount__sum'] or 0
+        balance = ins - outs
+        print('in %f and out %f, so new balance for %s is %f' % (ins, outs, account, balance))
+        account.balance = balance
+        account.save() 
+
+  
